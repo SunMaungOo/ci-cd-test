2b6ffea758dbb26ec7597be14a617e7451b04085
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com.>
2023-11-26 19:40:25 +0630

    test again

--- envtest.py
+++ envtest.py
@@ -1,9 +1,9 @@
 import os
 
 def main():
-    value = os.environ["TMP_VALUE"]
+    value = os.environ["TMP_IMPORTANT_VALUE"]
 
-    print(f"The value of TMP_VALUE is :{value}")
+    print(f"The value of TMP_IMPORTANT_VALUE is :{value}")
 
 
 if __name__=="__main__":
