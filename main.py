3b6a6699f9e227b446853f293a8e7ba7a555c904
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com.>
2023-11-26 19:15:16 +0630

    use calculator

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from calc import add_number
+
 def main():
-    print("Hello CICD")
+    a = 10 
+    b = 5
+
+    result = a+b
+    print(f"Result is :{result}")
+    
 
 if __name__=="__main__":
     main()
